692995cb8799022fee3aabbb46680214c37074de
Joao Viana <joaopadraoviana@gmail.com>
2021-05-10 21:45:21 +0100

    fix: remove unused packages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-    webpack (config) {
-        config.module.rules.push({
-            test: /\.md$/,
-            use: 'raw-loader'
-        });
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-          });
-      
-        return config;
-    }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
