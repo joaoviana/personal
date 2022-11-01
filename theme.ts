e7fd58f9d77710bc54f4be69dbe6384dc21424e3
Joao Viana <joaopadraoviana@gmail.com>
2022-11-01 11:35:44 +0000

    remove comment

--- theme.ts
+++ theme.ts
@@ -5,6 +5,4 @@ const config = {
   useSystemColorMode: false,
 };
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 export const theme = extendTheme({ config });
